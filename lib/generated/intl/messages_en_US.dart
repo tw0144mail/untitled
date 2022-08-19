57e20ba303b7a24f6c771a865cc12f60aa9f081a
zhuangkaiwen <tw0144mike@gmail.com>
2022-08-19 18:48:25 +0800

    版本問題

--- lib/generated/intl/messages_en_US.dart
+++ lib/generated/intl/messages_en_US.dart
@@ -7,7 +7,8 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -20,52 +21,70 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en_US';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "CustomCheckboxPage" : MessageLookupByLibrary.simpleMessage("CustomCheckboxPage"),
-    "CustomPaintRoutePage" : MessageLookupByLibrary.simpleMessage("CustomPaintRoutePage"),
-    "DoneWidgetTestPage" : MessageLookupByLibrary.simpleMessage("DoneWidgetTestPage"),
-    "DrawerPage" : MessageLookupByLibrary.simpleMessage("DrawerPage"),
-    "DynamicGenerationPage" : MessageLookupByLibrary.simpleMessage("DynamicGenerationPage"),
-    "FileOperationRoutePage" : MessageLookupByLibrary.simpleMessage("FileOperationRoutePage"),
-    "FutureBuilderRouteStatePage" : MessageLookupByLibrary.simpleMessage("FutureBuilderRouteStatePage"),
-    "GoTo" : MessageLookupByLibrary.simpleMessage("GoTo%sOWO"),
-    "GradientCircularPage" : MessageLookupByLibrary.simpleMessage("GradientCircularPage"),
-    "GridElementPage" : MessageLookupByLibrary.simpleMessage("GridElementPage"),
-    "GuidePage" : MessageLookupByLibrary.simpleMessage("GuidePage"),
-    "HttpTestRoutePage" : MessageLookupByLibrary.simpleMessage("HttpTestRoutePage"),
-    "ListPage" : MessageLookupByLibrary.simpleMessage("ListPage"),
-    "MixedPage" : MessageLookupByLibrary.simpleMessage("MixedPage"),
-    "NPressed" : MessageLookupByLibrary.simpleMessage("Pressed%s"),
-    "NextPage" : MessageLookupByLibrary.simpleMessage("NextPage"),
-    "PageOWO" : MessageLookupByLibrary.simpleMessage("PageOWO"),
-    "SocketRoutePage" : MessageLookupByLibrary.simpleMessage("SocketRoutePage"),
-    "StackComponentsPage" : MessageLookupByLibrary.simpleMessage("StackComponentsPage"),
-    "TabPage" : MessageLookupByLibrary.simpleMessage("TabPage"),
-    "TableElementPage" : MessageLookupByLibrary.simpleMessage("TableElementPage"),
-    "TurnBoxPage" : MessageLookupByLibrary.simpleMessage("TurnBoxPage"),
-    "WaterMarkPage" : MessageLookupByLibrary.simpleMessage("WaterMarkPage"),
-    "WaterMarkTestPage" : MessageLookupByLibrary.simpleMessage("WaterMarkTestPage"),
-    "WebSocketsPage" : MessageLookupByLibrary.simpleMessage("WebSocketsPage"),
-    "WithChunksPage" : MessageLookupByLibrary.simpleMessage("WithChunksPage"),
-    "app_name" : MessageLookupByLibrary.simpleMessage("陳凌-學習APP"),
-    "auto" : MessageLookupByLibrary.simpleMessage("auto"),
-    "cancel" : MessageLookupByLibrary.simpleMessage("cancel"),
-    "greet" : MessageLookupByLibrary.simpleMessage("greet~"),
-    "language" : MessageLookupByLibrary.simpleMessage("language"),
-    "login" : MessageLookupByLibrary.simpleMessage("login"),
-    "loginPage" : MessageLookupByLibrary.simpleMessage("loginPage"),
-    "logout" : MessageLookupByLibrary.simpleMessage("logout"),
-    "logoutTip" : MessageLookupByLibrary.simpleMessage("logoutTip"),
-    "myFormattedString" : MessageLookupByLibrary.simpleMessage("Hello %s"),
-    "noDescription" : MessageLookupByLibrary.simpleMessage("noDescription"),
-    "password" : MessageLookupByLibrary.simpleMessage("password"),
-    "passwordRequired" : MessageLookupByLibrary.simpleMessage("passwordRequired"),
-    "picktime" : MessageLookupByLibrary.simpleMessage("Pick a time"),
-    "theme" : MessageLookupByLibrary.simpleMessage("theme"),
-    "title" : MessageLookupByLibrary.simpleMessage("陳凌 Flutter測試 APP(英-美國)"),
-    "userName" : MessageLookupByLibrary.simpleMessage("userName"),
-    "userNameOrPasswordWrong" : MessageLookupByLibrary.simpleMessage("userNameOrPasswordWrong"),
-    "userNameRequired" : MessageLookupByLibrary.simpleMessage("userNameRequired"),
-    "yes" : MessageLookupByLibrary.simpleMessage("yes")
-  };
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "CustomCheckboxPage":
+            MessageLookupByLibrary.simpleMessage("CustomCheckboxPage"),
+        "CustomPaintRoutePage":
+            MessageLookupByLibrary.simpleMessage("CustomPaintRoutePage"),
+        "DoneWidgetTestPage":
+            MessageLookupByLibrary.simpleMessage("DoneWidgetTestPage"),
+        "DrawerPage": MessageLookupByLibrary.simpleMessage("DrawerPage"),
+        "DynamicGenerationPage":
+            MessageLookupByLibrary.simpleMessage("DynamicGenerationPage"),
+        "FileOperationRoutePage":
+            MessageLookupByLibrary.simpleMessage("FileOperationRoutePage"),
+        "FutureBuilderRouteStatePage":
+            MessageLookupByLibrary.simpleMessage("FutureBuilderRouteStatePage"),
+        "GoTo": MessageLookupByLibrary.simpleMessage("GoTo%sOWO"),
+        "GradientCircularPage":
+            MessageLookupByLibrary.simpleMessage("GradientCircularPage"),
+        "GridElementPage":
+            MessageLookupByLibrary.simpleMessage("GridElementPage"),
+        "GuidePage": MessageLookupByLibrary.simpleMessage("GuidePage"),
+        "HttpTestRoutePage":
+            MessageLookupByLibrary.simpleMessage("HttpTestRoutePage"),
+        "ListPage": MessageLookupByLibrary.simpleMessage("ListPage"),
+        "MixedPage": MessageLookupByLibrary.simpleMessage("MixedPage"),
+        "NPressed": MessageLookupByLibrary.simpleMessage("Pressed%s"),
+        "NextPage": MessageLookupByLibrary.simpleMessage("NextPage"),
+        "PageOWO": MessageLookupByLibrary.simpleMessage("PageOWO"),
+        "SocketRoutePage":
+            MessageLookupByLibrary.simpleMessage("SocketRoutePage"),
+        "StackComponentsPage":
+            MessageLookupByLibrary.simpleMessage("StackComponentsPage"),
+        "TabPage": MessageLookupByLibrary.simpleMessage("TabPage"),
+        "TableElementPage":
+            MessageLookupByLibrary.simpleMessage("TableElementPage"),
+        "TurnBoxPage": MessageLookupByLibrary.simpleMessage("TurnBoxPage"),
+        "WaterMarkPage": MessageLookupByLibrary.simpleMessage("WaterMarkPage"),
+        "WaterMarkTestPage":
+            MessageLookupByLibrary.simpleMessage("WaterMarkTestPage"),
+        "WebSocketsPage":
+            MessageLookupByLibrary.simpleMessage("WebSocketsPage"),
+        "WithChunksPage":
+            MessageLookupByLibrary.simpleMessage("WithChunksPage"),
+        "app_name": MessageLookupByLibrary.simpleMessage("陳凌-學習APP"),
+        "auto": MessageLookupByLibrary.simpleMessage("auto"),
+        "cancel": MessageLookupByLibrary.simpleMessage("cancel"),
+        "greet": MessageLookupByLibrary.simpleMessage("greet~"),
+        "language": MessageLookupByLibrary.simpleMessage("language"),
+        "login": MessageLookupByLibrary.simpleMessage("login"),
+        "loginPage": MessageLookupByLibrary.simpleMessage("loginPage"),
+        "logout": MessageLookupByLibrary.simpleMessage("logout"),
+        "logoutTip": MessageLookupByLibrary.simpleMessage("logoutTip"),
+        "myFormattedString": MessageLookupByLibrary.simpleMessage("Hello %s"),
+        "noDescription": MessageLookupByLibrary.simpleMessage("noDescription"),
+        "password": MessageLookupByLibrary.simpleMessage("password"),
+        "passwordRequired":
+            MessageLookupByLibrary.simpleMessage("passwordRequired"),
+        "picktime": MessageLookupByLibrary.simpleMessage("Pick a time"),
+        "theme": MessageLookupByLibrary.simpleMessage("theme"),
+        "title": MessageLookupByLibrary.simpleMessage("陳凌 Flutter測試 APP(英-美國)"),
+        "userName": MessageLookupByLibrary.simpleMessage("userName"),
+        "userNameOrPasswordWrong":
+            MessageLookupByLibrary.simpleMessage("userNameOrPasswordWrong"),
+        "userNameRequired":
+            MessageLookupByLibrary.simpleMessage("userNameRequired"),
+        "yes": MessageLookupByLibrary.simpleMessage("yes")
+      };
 }
